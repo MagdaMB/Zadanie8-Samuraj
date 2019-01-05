afd4e4b9eab4b2b372a65864b2baa4e0029141da
MagdaMB <magda.m.badura@gmail.com>
2019-01-05 20:04:45 +0100

    kursor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const spnText = document.querySelector(".text");
 
-const txt = "Et eu tempor esse nulla duis aliqua ut excepteur veniam ad culpa eiusmod. Cupidatat quis id dolor sit esse occaecat dolore nisi sit cillum aute deserunt velit. Sint elit excepteur voluptate minim et. Eu anim non cillum est nostrud nulla id ex ad. Incididunt qui occaecat ipsum laborum qui ut excepteur labore velit officia do officia consectetur. Thisis the END"
+const txt = "Et eu tempor esse nulla duis aliqua ut excepteur veniam ad culpa eiusmod. Cupidatat quis id dolor sit esse occaecat dolore nisi sit cillum aute deserunt velit. Sint elit excepteur voluptate minim et. Eu anim non cillum est nostrud nulla id ex ad. Thisis the END";
+
+const cursor = document.querySelector(".cursor");
 
 let indexText = 0;
 const addLetter = () => {
     spnText.textContent += txt[indexText];
+
     indexText++;
+
     if (indexText == txt.length) {
         clearInterval(indexTyping);
     }
 }
+
+const cursorAnimation = () => {
+    cursor.classList.toggle("visible");
+};
 const indexTyping = setInterval(addLetter, 50);
+
+setInterval(cursorAnimation, 400);
